1ed77514068f7906f1cb50d844e86043208badd0
Eitan Lichtenstein <eitanlich2000@gmail.com>
2023-02-20 20:58:23 +0200

    Updated column from SKU to UPC

--- removeDB.php
+++ removeDB.php
@@ -9,8 +9,8 @@ $con = mysqli_connect(HOSTNAME, USERNAME, PASSWORD, DB);
 if (isset(
     $_POST['item-sku'],
 )) {
-    $item_sku = $_POST['item-sku'];
-    $query = "DELETE  FROM items WHERE item_sku = '$item_sku'";
+    $item_sku = $_POST['item-upc'];
+    $query = "DELETE  FROM items WHERE item_upc = '$item_upc'";
     mysqli_query($con, $query);
     header("Location:index.php");
 }
