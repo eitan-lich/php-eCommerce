20cf8090d184e6bf5c22f2b1fb97c21922fa5ff9
Eitan Lichtenstein <eitanlich2000@gmail.com>
2023-02-21 13:36:32 +0200

    Updated to UPC instead of SKU

--- removeDB.php
+++ removeDB.php
@@ -7,9 +7,9 @@ $con = mysqli_connect(HOSTNAME, USERNAME, PASSWORD, DB);
 
 
 if (isset(
-    $_POST['item-sku'],
+    $_POST['item-upc'],
 )) {
-    $item_sku = $_POST['item-upc'];
+    $item_upc = $_POST['item-upc'];
     $query = "DELETE  FROM items WHERE item_upc = '$item_upc'";
     mysqli_query($con, $query);
     header("Location:index.php");
